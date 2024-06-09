cb4c0b9c85f4e2218b953f851bd984ec7d19a1ed
hossein-mo <hossein.mo.c@gmail.com>
2024-06-09 17:42:17 +0300

    hotfix: included faculty urls in main urls.py

--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("user/", include("user.urls")),
+    path("faculty/", include("faculty.urls")),
 ]
